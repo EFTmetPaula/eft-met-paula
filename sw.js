4339cd67f3c1822d6d388be741152c40b9d9a5fd
EFTmetPaula <info@paulasangers.nl>
2025-11-04 13:27:08 +0100

    Service Worker compleet uitgeschakeld - geen cache meer

--- sw.js
+++ sw.js
@@ -1,83 +1,35 @@
-const CACHE_NAME = 'eft-paula-v3';
-const urlsToCache = [
-  './',
-  './index.html',
-  './wat-is-eft.html',
-  './kloppunten.html',
-  './eft-tapping.html',
-  './eindpagina.html',
-  './extra-hulp.html',
-  './disclaimer.html',
-  './styles.css',
-  './manifest.json',
-  './logo/logo-avatar-zonderbg.png',
-  './avatar/karatepunt.jpg?v=3',
-  './avatar/kruin.jpg?v=3',
-  './avatar/wenkbrauw.jpg?v=3',
-  './avatar/zijkant-oog.jpg?v=3',
-  './avatar/onder-oog.jpg?v=3',
-  './avatar/onder-neus.jpg?v=3',
-  './avatar/kin.jpg?v=3',
-  './avatar/sleutelbeen.jpg?v=3',
-  './avatar/onder-arm.jpg?v=3',
-  './avatar/logo-avatar.jpg',
-  './avatar/uitleg-eft.mp4'
-];
+// Deze Service Worker is UITGESCHAKELD
+// Hij verwijdert zichzelf en alle caches
 
-// Install event - cache resources
 self.addEventListener('install', event => {
+  // Verwijder alle caches
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Fetch event - serve from cache when offline
-self.addEventListener('fetch', event => {
-  const request = event.request;
-  const url = new URL(request.url);
-  
-  // Zorg ervoor dat de root URL altijd naar index.html gaat
-  if (url.pathname === '/' || url.pathname === '/eft-met-paula/' || url.pathname === '/eft-met-paula') {
-    event.respondWith(
-      caches.match('./index.html')
-        .then(response => {
-          return response || fetch('./index.html');
+    caches.keys().then(cacheNames => {
+      console.log('SW: Verwijder alle caches');
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          return caches.delete(cacheName);
         })
-    );
-    return;
-  }
-  
-  event.respondWith(
-    caches.match(request)
-      .then(response => {
-        // Return cached version or fetch from network
-        return response || fetch(request);
-      })
-      .catch(() => {
-        // Fallback voor offline gebruik
-        if (request.destination === 'document') {
-          return caches.match('./index.html');
-        }
-      })
+      );
+    }).then(() => {
+      // Unregister deze service worker
+      return self.registration.unregister();
+    })
   );
+  self.skipWaiting();
 });
 
-// Activate event - clean up old caches
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
+          return caches.delete(cacheName);
         })
       );
+    }).then(() => {
+      return self.registration.unregister();
     })
   );
+  self.clients.claim();
 }); 
